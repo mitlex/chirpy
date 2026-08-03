9c0f69623a12c354c2167eb23596da11b3703a1a
mitlex <94235400+mitlex@users.noreply.github.com>
2026-08-03 14:55:05 +0100

    feat: initialize basic HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// create new http.ServeMux to route requests
+	httpReqRouter := http.NewServeMux()
+
+	// create http server
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: httpReqRouter, // server passes requests to this master Handler, which then routes the requests based on the URL
+	}
+
+	// start the server, listening on Addr (:8080)
+	// only returns error when it stops or fails to start
+	// it blocks the goroutine (main) it's running in for the life of the server hence we place it at the end of our main func
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err) // this also calls os.Exit(1) terminating the program immediately; if server can't run, nothing left for main to do
+	}
 }
