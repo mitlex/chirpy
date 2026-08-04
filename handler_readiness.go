7ea64e5de1fcf5b9de181352a4d74f6404e42b59
mitlex <94235400+mitlex@users.noreply.github.com>
2026-08-04 15:55:36 +0100

    feat: add readiness endpoint /healthz, update fileserver to /app
    Also includes Chirpy logo asset for the fileserver.

---
+++ handler_readiness.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+// serverReadinessEndpoint is called by server upon receiving "/healthz" in URL path
+// It simply sets and responds with a plain text utf-8 charset Content-Type header and a 200 status code and "OK" body
+// It is used to indicate whether or not our server is ready to receive traffic
+func serverReadinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	// Header represents the key:value pairs in an HTTP Header map[string][]string
+	// We set the Content-Type header using .Set()
+	// Nothing is sent to the socket yet, the header is simply buffered
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	// Flush status 200 and all buffered headers to the socket
+	w.WriteHeader(http.StatusOK)
+
+	// Stream the body bytes
+	w.Write([]byte("OK"))
+}
--- main.go
+++ main.go
@@ -11,12 +11,23 @@ func main() {
 	httpReqRouter := http.NewServeMux()
 
 	// Register handlers for requests
+
+	// FileServer
 	// WARNING: http.Dir(".") allows the handler to serve any file from the root directory
-	// 			fine for this toy project, but better practice to create a dedicated public folder (i.e. http.Dir("public")) to place files we'd like to expose
-	// NOTE #1 : FileServer will automatically serve index.html if present in the directory, OR, if not present, serves a listing of all files in the directory
-	// NOTE #2 : http.Dir(".") converts the string "." to an http.Dir type.
-	// 			 http.Dir implements the http.FileSystem interface, allowing http.FileServer to serve files from the current directory (.).
-	httpReqRouter.Handle("/", http.FileServer(http.Dir(".")))
+	// 			Fine for this toy project, but better practice to create a dedicated public folder (i.e. http.Dir("public")) to place files we'd like to expose
+	// NOTE #1: FileServer will automatically serve index.html if present in the directory, OR, if not present, serves a listing of all files in the directory
+	// NOTE #2: http.Dir(filepathRoot) converts the string "." to an http.Dir type
+	// 			http.Dir implements the http.FileSystem interface, allowing http.FileServer to serve files from the current directory (root: .)
+	//			In other words, http.Dir tells the FileServer where on disk to look for files
+	// NOTE #3: StripPrefix strips `/app` from the URL path before FileServer looks for it on disk
+	// 			Example: URL request /app/assets/logo.png becomes /assets/logo.png which is where the logo.png file actually lives on disk (reminder: we don't have an /app folder on disk)
+	// 			This allows us to create a /app namespace that the httpReqRouter Mux can route /app URL traffic to, even though the FileServer is serving files from root (.) directory.
+	// 			If we continued to register the FileServer at "/" then any requests like /healthz would be routed to the FileServer as well, as "/" swallows all other requests like "/x", "/x/y"
+	const filepathRoot = "."
+	httpReqRouter.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	// Readiness endpoint (/healthz)
+	httpReqRouter.HandleFunc("/healthz", serverReadinessEndpoint)
 
 	// create http server
 	srv := &http.Server{
